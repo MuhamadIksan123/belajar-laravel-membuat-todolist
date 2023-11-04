ffaee3aa9b2da5472112c6a774c8f07a23b6619a
Muhamad Iksan <muhamadiksan351@gmail.com>
2023-11-04 07:47:37 +0700

    membuat project todo-list

---
+++ app/Providers/UserServiceProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Providers;
+
+use App\Services\Impl\UserServiceImpl;
+use App\Services\UserService;
+use Illuminate\Support\ServiceProvider;
+
+class UserServiceProvider extends ServiceProvider
+{
+    public array $singletons = [
+        UserService::class => UserServiceImpl::class
+    ];
+
+    public function provides(): array
+    {
+        return [UserService::class];
+    }
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        return [UserService::class];
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
